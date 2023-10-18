1e19d79115d1836706370c91cc3d401ef46d3fa6
Gagan Deep Kashyap <cu.gagankashyap@gmail.com>
2023-10-18 10:33:13 +0530

    today change

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,12 @@ Route::get('/', function () {
     return $response->body(); // This will fetch content from Node.js server (public/views/index.html)
 });
 
-Route::get('/about', function() {
-    $response = Http::get('http://localhost:3000/about');
+// Route::get('/about', function() {
+//     $response = Http::get('http://localhost:3000/about');
+//     return $response->body();
+// });
+
+Route::get('/{page_name}', function($page_name) {
+    $response = Http::get("http://localhost:3000/$page_name");
     return $response->body();
 });
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -10,20 +9,26 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Middleware to add header to all responses
 app.use((req, res, next) => {
     res.headerContent = fs.readFileSync(path.join(__dirname, 'public', 'views', 'header.html'), 'utf8');
+    res.footerContent = fs.readFileSync(path.join(__dirname, 'public', 'views', 'footer.html'), 'utf8');
     next();
 });
 
-// Handle requests to all HTML files and prepend the header
 app.use((req, res, next) => {
-    if (req.url !== '/favicon.ico') { // Exclude favicon request
-        const filePath = path.join(__dirname, 'public', 'views', req.url + '.html');
+    if (req.url !== '/favicon.ico') { 
+         if (req.url === '/') {
+            filePath = path.join(__dirname, 'public', 'views', 'index.html');
+        } else {
+            filePath = path.join(__dirname, 'public', 'views', req.url + '.html');
+        }
+
+        //const filePath = path.join(__dirname, 'public', 'views', req.url + '.html');
         fs.readFile(filePath, 'utf8', (err, content) => {
             if (err) {
                 // Handle error, for example, send a 404 response
                 res.status(404).send('File not found');
             } else {
                 // Prepend header content to the file content
-                res.send(res.headerContent + content);
+                res.send(res.headerContent + content + res.footerContent);
             }
         });
     } else {
@@ -35,36 +40,4 @@ app.listen(port, () => {
     console.log(`Node.js server is running at http://localhost:${port}`);
 });
 
-
-// const express = require('express');
-// const path = require('path');
-// const bodyParser = require('body-parser');
-// const app = express();
-// const port = 3000;
-
-// // Require route files
-// const indexRoute = require('./routes/index');
-// const aboutRoute = require('./routes/about');
-
-// // Use routes
-// // app.use('/', indexRoute);
-// // app.use('/about', aboutRoute);
-// // Serve static files from the public folder
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// // Handle requests to the root URL
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'views', 'index.html'));
-// });
-// app.get('/about', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'views', 'about.html'));
-// });
-
-// app.get('/service', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'views', 'service.html'));
-// });
-
-// app.listen(port, () => {
-//     console.log(`Node.js server is running at http://localhost:${port}`);
-// });
 
