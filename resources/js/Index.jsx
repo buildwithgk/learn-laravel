f686c8cdf9a34024ecb2927ec96de9d5ebaded80
Gagan Deep Kashyap <cu.gagankashyap@gmail.com>
2023-10-27 10:24:50 +0530

    change

--- resources/js/Index.jsx
+++ resources/js/Index.jsx
@@ -374,7 +374,7 @@ export default function Index() {
              
             <div className="container-fluid py-5 mb-5 team">
                 <div className="container">
-                    <div className="text-center mx-auto pb-5 wow fadeIn" data-wow-delay=".3s" style="max-width: 600px;">
+                    <div className="text-center mx-auto pb-5 wow fadeIn" data-wow-delay=".3s" style={{ maxwidth: "600px" }}>
                         <h5 className="text-primary">Our Team</h5>
                         <h1>Meet our expert Team</h1>
                     </div>
@@ -461,7 +461,7 @@ export default function Index() {
 
             <div className="container-fluid testimonial py-5 mb-5">
                 <div className="container">
-                    <div className="text-center mx-auto pb-5 wow fadeIn" data-wow-delay=".3s" style="max-width: 600px;">
+                    <div className="text-center mx-auto pb-5 wow fadeIn" data-wow-delay=".3s" style={{ maxwidth: "600px" }}>
                         <h5 className="text-primary">Our Testimonial</h5>
                         <h1>Our Client Saying!</h1>
                     </div>
@@ -556,7 +556,7 @@ export default function Index() {
 
             <div className="container-fluid py-5 mb-5">
                 <div className="container">
-                    <div className="text-center mx-auto pb-5 wow fadeIn" data-wow-delay=".3s" style="max-width: 600px;">
+                    <div className="text-center mx-auto pb-5 wow fadeIn" data-wow-delay=".3s" style={{ maxwidth: "600px" }}>
                         <h5 className="text-primary">Get In Touch</h5>
                         <h1 className="mb-3">Contact for any query</h1>
                         <p className="mb-2">The contact form is currently inactive. Get a functional and working contact form with Ajax & PHP in a few minutes. Just copy and paste the files, add a little code and you're done. <a href="https://htmlcodex.com/contact-form">Download Now</a>.</p>
@@ -565,7 +565,7 @@ export default function Index() {
                         <div className="row g-5 mb-5 justify-content-center">
                             <div className="col-xl-4 col-lg-6 wow fadeIn" data-wow-delay=".3s">
                                 <div className="d-flex bg-light p-3 rounded">
-                                    <div className="flex-shrink-0 btn-square bg-secondary rounded-circle" style="width: 64px; height: 64px;">
+                                    <div className="flex-shrink-0 btn-square bg-secondary rounded-circle" style={{ width: "64px", height: "64px" }}>
                                         <i className="fas fa-map-marker-alt text-white"></i>
                                     </div>
                                     <div className="ms-3">
@@ -576,7 +576,7 @@ export default function Index() {
                             </div>
                             <div className="col-xl-4 col-lg-6 wow fadeIn" data-wow-delay=".5s">
                                 <div className="d-flex bg-light p-3 rounded">
-                                    <div className="flex-shrink-0 btn-square bg-secondary rounded-circle" style="width: 64px; height: 64px;">
+                                    <div className="flex-shrink-0 btn-square bg-secondary rounded-circle" style={{ width: "64px", height: "64px" }}>
                                         <i className="fa fa-phone text-white"></i>
                                     </div>
                                     <div className="ms-3">
@@ -587,7 +587,7 @@ export default function Index() {
                             </div>
                             <div className="col-xl-4 col-lg-6 wow fadeIn" data-wow-delay=".7s">
                                 <div className="d-flex bg-light p-3 rounded">
-                                    <div className="flex-shrink-0 btn-square bg-secondary rounded-circle" style="width: 64px; height: 64px;">
+                                    <div className="flex-shrink-0 btn-square bg-secondary rounded-circle" style={{ width: "64px", height: "64px" }}>
                                         <i className="fa fa-envelope text-white"></i>
                                     </div>
                                     <div className="ms-3">
@@ -600,7 +600,7 @@ export default function Index() {
                         <div className="row g-5">
                             <div className="col-lg-6 wow fadeIn" data-wow-delay=".3s">
                                 <div className="p-5 h-100 rounded contact-map">
-                                    <iframe className="rounded w-100 h-100" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3025.4710403339755!2d-73.82241512404069!3d40.685622471397615!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c26749046ee14f%3A0xea672968476d962c!2s123rd%20St%2C%20Queens%2C%20NY%2C%20USA!5e0!3m2!1sen!2sbd!4v1686493221834!5m2!1sen!2sbd" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                                    <iframe className="rounded w-100 h-100" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3025.4710403339755!2d-73.82241512404069!3d40.685622471397615!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c26749046ee14f%3A0xea672968476d962c!2s123rd%20St%2C%20Queens%2C%20NY%2C%20USA!5e0!3m2!1sen!2sbd!4v1686493221834!5m2!1sen!2sbd" style={{ border:"0" }} loading="lazy"></iframe>
                                 </div>
                             </div>
                             <div className="col-lg-6 wow fadeIn" data-wow-delay=".5s">
